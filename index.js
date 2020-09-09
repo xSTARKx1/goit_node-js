673e1a274a184ea8c23589e066f29ae78adb1c5a
Viktor Karmazeniuk <victor.k9560@gmail.com>
2020-09-09 21:04:53 +0300

    homework 1

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log("asdasd");
+const contacts = require("./contacts.js");
+const argv = require("yargs").argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case "list":
+      contacts.listContacts().then((res) => console.table(res));
+      break;
+
+    case "get":
+      contacts.getContactById(id).then((res) => console.log(res));
+      break;
+
+    case "add":
+      contacts.addContact(name, email, phone).then((res) => console.log(res));
+      break;
+
+    case "remove":
+      contacts.removeContact(id).then((res) => console.log(res));
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+}
+invokeAction(argv);
